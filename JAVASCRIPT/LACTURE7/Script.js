69e06a100103e8d715dfc0e03d540b08bccfaf2d
abhi bhalala <skillabhi026@gamil.com>
2024-07-05 09:50:49 -0700

    JS

---
+++ JAVASCRIPT/LACTURE7/Script.js
@@ -0,0 +1,95 @@
+// for of loop 
+
+{
+//     let arr = ['js' , 'hmtl' , 'java' , 'py' , 'nextjs' , 'css' , 'nodejs' , ]
+    
+// console.log(arr);
+
+//     for (const item of arr) {
+//         console.log(arr);
+        
+//     }
+
+//     arr.forEach((item)  => console.log('forEach'   ,item + 10)) 
+
+//     let num = [1 ,6 ,2 ,4, 5]
+
+//     for (const multi of num) {
+//         console.log(num * 10 )
+        
+//     }
+
+//     num.forEach((item) =>  console.log( item  + 10))
+//     num.map((item) => console.log(item + '10'))
+
+    
+}
+
+// bacis function 
+// {
+// let fun1 = function Vote()
+// {
+//     let number = prompt("Number ");
+
+//     if (number%5==0)
+//         {
+//             console.log("number is divisible by 5");
+//         }else if (number%3==0){
+//             console.log("number is not divisible by 3");
+//         }else
+//         {
+//             console.log("number is not divisible by 5 and 3");
+//         }
+
+// }
+// // fun1()
+// console.log(fun1())
+// }
+
+
+// arrow function
+
+// let number = 4;
+
+// let fun2 = () => number = prompt("Number ") 
+
+// console.log(fun2());
+
+
+//  function with return 
+// {
+
+// let fun3 = function sum (){
+//     return 5 * 5
+// }
+// console.log(fun3())
+
+
+
+// }
+
+
+// html to js
+
+{
+    function html(){
+        document.getElementById('demo').innerHTML = 'hi html'
+
+    }
+
+
+    function css ()
+    {
+        document.getElementById('demo').style.backgroundColor = 'red'; 
+        document.getElementById('demo').style.color = 'blue'; 
+        document.getElementById('demo').style.fontSize = '40px'; 
+        document.getElementById('demo').style.textAlign = 'center'
+        document.getElementById('demo').style.translate = "95px"
+
+
+
+    }
+
+
+}
+
---
+++ JAVASCRIPT/String_methods/Script.js
@@ -0,0 +1,74 @@
+// string methods
+
+// 1. trim (start and end ni spase remove kare)
+// 2. TrimEnd(string ni end ni spase remove kare che)
+// 3. TrimStart(string ni start ni spase remove kare che)
+
+
+{
+    //     let str =  '                            abhi bhalala                                     ';
+
+    //     // trim
+
+    //    str1 =  str.trim();
+
+    //     console.log(str1)
+
+    //     // trimEnd
+
+    //     str2 = str.trimEnd()
+
+    //     console.log(str2);
+
+    //     // trimStart
+
+
+    //     str3 = str.trimStart()
+
+    //     console.log()
+}
+
+
+// 4. string charAt(index) je index apne apisu ae index uper je charater hase aene print kri apse
+// 5. charCodeat(index)
+
+{
+    // let str = "   abhi is best    "
+
+    // str1 = str.charAt(5)
+
+    // console.log(str1)
+
+
+    // str2 = str.charCodeAt(5)  // je index apne apisu ae index uper je charater hase teni ascii value print kari apse.
+
+    // console.log(str2)
+
+
+}
+
+
+//6. string.at ()
+
+{
+    //     let str = "   abhi is best    "
+
+    //     str1 = str.at(-5) // CharAt() ma  negitive index not posibale but at()  negitive index  posible 
+
+    //     console.log(str1)
+
+}
+
+
+/*  7. includes(searchString)
+     includes(searchString, position)  */
+
+// {
+//     let str = 'Abhi bhalala!  ASDFGHJHFDFGHGFFGHGGoHGHHH';
+
+//     str1 = str.includes('Abhi', 1)
+
+//     console.log(str1)
+
+
+// }
