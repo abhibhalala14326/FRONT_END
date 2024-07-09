66948fd52da9ab02005cecb7067f4e6979752fdb
abhi bhalala <skillabhi026@gamil.com>
2024-07-09 10:10:15 -0700

    JS

---
+++ JAVASCRIPT/Math/Script.js
@@ -0,0 +1,32 @@
+// math object
+
+{
+    // console.log(Math.E)
+    // console.log(Math.PI)
+    // console.log(Math.SQRT2)
+    // console.log(Math.SQRT1_2)
+    // console.log(Math.LN2)
+    // console.log(Math.LOG10E)
+    // console.log(Math.LOG2E)
+}
+
+//math methods
+
+{
+    console.log(Math.ceil(2.6))
+    console.log(Math.floor(-2.3))
+    console.log(Math.trunc(0.56787654))
+    console.log(Math.pow(2, 4))
+    console.log(Math.trunc(Math.random()* 100))
+    console.log(Math.round(9.9876543))
+    console.log(Math.max(1,3,4,5,6,8765,123456,9,100))
+    console.log(Math.min(1,3,4,5,6,8765,123456,9,100))
+    console.log(Math.sign(-19))
+    console.log(Math.trunc(Math.sqrt(16)))
+    console.log(Math.abs(-4.5))
+    console.log(Math.sin(90 * Math.PI / 180))
+    console.log(Math.cos(90 * Math.PI / 180))
+    console.log(Math.trunc(Math.log2(1000)))
+    console.log(Math.log10(10000))
+
+}
--- JAVASCRIPT/String_methods/Script.js
+++ JAVASCRIPT/String_methods/Script.js
@@ -72,3 +72,164 @@
 
 
 // }
+
+// string methods
+
+//8. slice
+//9 .slice(start,  end)index
+
+
+{
+
+    let str = " abhishek bhalala  "
+
+    // str1= str.slice(10)
+
+    // console.log(str1)
+
+    // str2 = str.slice(1 ,-7)
+
+    // console.log(str2)
+
+}
+
+
+// 10. substring .
+// 11. substring (start , end )  (index).
+
+
+{
+    // let str = " abhishek bhalala  "
+
+    // str1 = str.substring(6 , 18)  //negitive index  lese nai (0)
+
+    // console.log(str1)
+}
+
+//12.concat()
+
+{
+    // let str1 = " abhishek bhalala  " ;
+    // let str2 = " bhalala  " ;
+    // let str3 = " " ;
+
+
+    // let strmain = str1.concat(str3, str2 )
+
+    // console.log(strmain)
+
+
+
+}
+
+
+
+//13. stratwith 
+//14. endwith
+
+
+{
+    //     let str = "  abhishek bhalala   ! @#$%%^^ "
+    //                012345678901234567890123456789
+    // str1 = str.startsWith("!" ,21 )
+    // str2 = str.endsWith("!" 19 )
+
+    // console.log(str1, str2)
+
+}
+
+
+
+//15. indexof(search , position)
+
+{
+    // let str = "  abhishek bhalala    "
+    //            0123456789012345678901
+    // let str1 = str.indexOf('h' , 9)
+
+    // console.log(str1)
+
+}
+// 16. padstart
+// 17. padend
+
+{
+    // let str = "  abhishek bhalala   "
+
+    // str1 = str.padStart(25 , 0)
+    // str2 = str.padEnd(25 , 0)
+
+    // console.log(str1)
+    // console.log(str2)
+
+}
+
+
+//18. repeat 
+
+{
+    // let str = "  abhishek   "
+
+    // str1 = str.repeat(5)
+
+    // console.log(str1)
+
+}
+
+//20. touppercase
+//21. tolowercase
+
+{
+    //         let str = "  Abhishek bhalala    "
+
+    //         let str2 = str.toUpperCase()
+
+    //     let str3 = str.toLowerCase()
+
+    //          console.log(str2)
+    //          console.log(str3)
+
+
+
+}
+
+
+// 22. split(separator)
+// 23. split(separator, limit)
+
+
+{
+    // let str = "  Abhishek bhalala    "
+
+
+    // let str1 = str.split(" ")
+    // let str2 = str.split(" " , 4)
+
+    // console.log(str1)
+    // console.log(str2)
+
+}
+
+
+
+//24. value of
+{
+    //     let str = "  Abhishek bhalala    "
+
+    // let str1 = new String ('name is so small')
+
+    // str2 = str.valueOf()
+
+    // str3 = str1.valueOf()
+
+
+    // console.log(str2)
+    // console.log(str3)
+
+    // console.log(typeof(str2))
+    // console.log(typeof(str3))
+
+    // console.log(typeof(str1))
+    // console.log(typeof(str))
+
+}
